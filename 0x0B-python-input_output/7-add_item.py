48741ff257adee0e535778e95e0d3e6a4e2dd2fd
Photolo <photolo.gift@gmail.com>
2024-03-04 09:17:21 -0800

    new code

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,18 +1,14 @@
 #!/usr/bin/python3
-from sys import argv
-"""
-Adds all arguments to a Python list, and then save them to a file
-"""
-
+import json
+import sys
+import os.path as path
 
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-try:
-    arg = load_from_json_file("add_item.json")
-except:
-    arg = []
-for i in range(1, len(argv)):
-    arg.append(argv[i])
-
-save_to_json_file(arg, "add_item.json")
+basic_entry = []
+if path.exists('add_item.json') is True:
+    basic_entry = load_from_json_file('add_item.json')
+for i in range(1, len(sys.argv)):
+    basic_entry.append(sys.argv[i])
+save_to_json_file(basic_entry, 'add_item.json')
